98fa1de2368f66b378594682ab2bdff495dfb3aa
Karahan Buhan <iletisim@karahanbuhan.com>
2025-08-06 20:05:46 +0300

    Fix proxy

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import logging
+from werkzeug.middleware.proxy_fix import ProxyFix
 from flask import Flask, session, redirect, url_for
 from routes.main import main_bp
 from routes.serve import serve_bp
@@ -11,6 +12,9 @@ logging.basicConfig(
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile("config.py")
+
+    # Fix https urls (if you are using http and not tls, comment the line below!)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
     app.secret_key = app.config["SECRET_KEY"]
 
